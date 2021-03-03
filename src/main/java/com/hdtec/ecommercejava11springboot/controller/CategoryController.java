b3795e40a13e912b3c75d9c811c4752837a161f3
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-03 11:05:59 -0300

    Testing first class - Category

--- src/main/java/com/hdtec/ecommercejava11springboot/controller/CategoryController.java
+++ src/main/java/com/hdtec/ecommercejava11springboot/controller/CategoryController.java
@@ -1,15 +1,28 @@
 package com.hdtec.ecommercejava11springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hdtec.ecommercejava11springboot.model.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryController {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST is working!";
+	public List<Category> list() {
+
+		Category cat1 = new Category(1, "computing");
+		Category cat2 = new Category(2, "office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
