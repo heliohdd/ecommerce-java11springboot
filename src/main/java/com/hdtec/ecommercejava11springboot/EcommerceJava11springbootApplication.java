fac52bcb6f06a60bf595a56987c012a8867c0056
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-03 18:25:34 -0300

    Instantiate objects Category

--- src/main/java/com/hdtec/ecommercejava11springboot/EcommerceJava11springbootApplication.java
+++ src/main/java/com/hdtec/ecommercejava11springboot/EcommerceJava11springbootApplication.java
@@ -1,13 +1,31 @@
 package com.hdtec.ecommercejava11springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceJava11springbootApplication {
+import com.hdtec.ecommercejava11springboot.model.Category;
+import com.hdtec.ecommercejava11springboot.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class EcommerceJava11springbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceJava11springbootApplication.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
 
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
