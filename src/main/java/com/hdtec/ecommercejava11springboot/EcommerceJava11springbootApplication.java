36cf9f756e28708672b6f65bfb65a40d94eee5fb
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-03 10:06:47 -0300

    Created Project

---
+++ src/main/java/com/hdtec/ecommercejava11springboot/EcommerceJava11springbootApplication.java
@@ -0,0 +1,13 @@
+package com.hdtec.ecommercejava11springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceJava11springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceJava11springbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdtec/ecommercejava11springboot/EcommerceJava11springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdtec.ecommercejava11springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJava11springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
