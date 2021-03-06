35f1fe30deef4da99a3ddee798ca206b906defa3
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-06 09:41:23 -0300

    POST Method to insert new Category register

--- src/main/java/com/hdtec/ecommercejava11springboot/controller/CategoryController.java
+++ src/main/java/com/hdtec/ecommercejava11springboot/controller/CategoryController.java
@@ -1,27 +1,38 @@
 package com.hdtec.ecommercejava11springboot.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hdtec.ecommercejava11springboot.model.Category;
 import com.hdtec.ecommercejava11springboot.services.CategoryService;
 
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryController {
 
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 
 		Category obj = categoryService.find(id);
 		return ResponseEntity.ok().body(obj);
-		
+
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = categoryService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/hdtec/ecommercejava11springboot/services/CategoryService.java
+++ src/main/java/com/hdtec/ecommercejava11springboot/services/CategoryService.java
@@ -20,4 +20,9 @@ public class CategoryService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
+
+	public Category insert(Category obj) {
+		obj.setId(null);
+		return categoryRepository.save(obj);
+	}
 }
